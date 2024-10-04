7350f4f0a2b1c74c108f36c288c15d2a27fa560c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-04 16:05:27 -0300

    create class Cliente

--- Dominio/Entidades/Cliente.cs
+++ Dominio/Entidades/Cliente.cs
@@ -8,5 +8,15 @@ namespace CrudCliente.Dominio.Entidades
 {
     internal class Cliente
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string  Email { get; set; }
+        public DateTime DataNascimento  { get; set; }
+        public bool  Ativo { get; set; }
+
+        public Cliente()
+        {
+            Ativo = true;
+        }
     }
 }
