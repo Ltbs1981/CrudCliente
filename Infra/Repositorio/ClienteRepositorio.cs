d900d4f799602924eb4f0c33fc19f5e53112cecc
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-06 17:55:50 -0300

    implementada os métodos nas classes ClienteRepositury, e ClienteService

--- Infra/Repositorio/ClienteRepositorio.cs
+++ Infra/Repositorio/ClienteRepositorio.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrudCliente.Dominio.Entidades;
 
 namespace CrudCliente.Infra.Repositorio
 {
     internal class ClienteRepositorio
     {
+        private static List<Cliente> clientes = new List<Cliente>();
+        private static int id = 0;
+
+        public void Add(Cliente cliente)
+        {
+            cliente.Id = id++;
+            clientes.Add(cliente);
+        }
+
+        public void Update(Cliente cliente)
+        {
+            Cliente banco = clientes.FirstOrDefault(x => x.Id == cliente.Id);
+        }
+
+        public void DeleteFisico(Cliente cliente)
+        {
+            clientes.Remove(cliente);
+        }
+
+        public void DeleteLogico(int id)
+        {
+            var cliente = clientes.FirstOrDefault(x => x.Id == id);
+            cliente.Ativo = false;
+        }
+
+        public List<Cliente> GetClientes()
+        {
+            return clientes;
+        }
+
+        public List<Cliente> ListarClientesAtivos()
+        {
+            return clientes.Where(x => x.Ativo == true).ToList();
+        }
+
     }
 }
--- Servicos/ClienteService.cs
+++ Servicos/ClienteService.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrudCliente.Dominio.Entidades;
+using CrudCliente.Infra.Repositorio;
 
 namespace CrudCliente.Servicos
 {
     internal class ClienteService
     {
+        private ClienteRepositorio _clienteRepositorio;
+
+        public void Add(Cliente cliente)
+        {
+            if (!ValidarCliente(cliente))
+                Console.WriteLine("Erro ao adicionar cliente");
+            _clienteRepositorio = new ClienteRepositorio();
+            _clienteRepositorio.Add(cliente);
+        }
+
+        public List<Cliente> GetAll()
+        {
+            _clienteRepositorio = new ClienteRepositorio();
+            return _clienteRepositorio.GetClientes();
+        }
+
+        private bool ValidarCliente(Cliente cliente)
+        {
+            if (cliente == null)
+                return false;
+            else if (string.IsNullOrWhiteSpace(cliente.Nome) &&
+                    string.IsNullOrWhiteSpace(cliente.Email) &&
+                    cliente.DataNascimento != DateTime.MinValue)
+                return false;
+            return true;
+        }
     }
 }
